b21cb821ff7d1299de6f7e2c47afc0ac74f57330
dapurv5@gmail.com <dapurv5@gmail.com@12eac49d-31b5-e0a3-7e2a-d932c23f927b>
2012-03-06 02:36:43 +0000

    assignment 6 complete

--- trunk/src/assignment6/A6Driver.java
+++ trunk/src/assignment6/A6Driver.java
@@ -8,8 +8,6 @@ import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
-import assignment5.LowPassFilter;
-
 public class A6Driver {
 
   /**
@@ -18,26 +16,43 @@ public class A6Driver {
    */
   public static void main(String[] args) throws IOException {
 
-  String name = "wdg2.gif";
-    
-  System.out.println(Arrays.toString(args));
-  
-//  if(args.length < 1){
-//    System.out.println("Usage: java -jar Filename.jpg");
-//    System.out.println("Exiting ...");
-//    System.exit(0);
-//  }
-//
-//  
-//  String name = args[0];
+    //    String name = "rgb.jpeg";
+    //    String name = "woman_blonde.tif";
 
-  BufferedImage image      = ImageIO.read(new File(name));
-  
-  BufferedImageOp segmentOp  = new SegmentationOp();
-  BufferedImage segmentedImg = segmentOp.filter(image, null);   
-  File file1 = new File("segmented-"+name);
-  ImageIO.write(segmentedImg, "gif", file1);
+    System.out.println(Arrays.toString(args));
 
+    if(args.length < 1){
+      System.out.println("Usage: java -jar Filename.jpg");
+      System.out.println("Exiting ...");
+      System.exit(0);
+    }
+
+
+    String name = args[0];
+
+    String[] arr = name.split("\\.");
+    String format = arr[1];
+
+
+
+    BufferedImage image      = ImageIO.read(new File(name));
+
+    SegmentationOp segmentOp  = new SegmentationOp();
+
+    segmentOp.setMode(ColorMode.RED);
+    BufferedImage segmentedImg = segmentOp.filter(image, null);
+    File file1 = new File("redSegmented-"+name);
+    ImageIO.write(segmentedImg, format, file1);
+
+    segmentOp.setMode(ColorMode.GREEN);
+    segmentedImg = segmentOp.filter(image, null);
+    file1 = new File("greenSegmented-"+name);
+    ImageIO.write(segmentedImg, format, file1);
+
+    segmentOp.setMode(ColorMode.BLUE);
+    segmentedImg = segmentOp.filter(image, null);
+    file1 = new File("blueSegmented-"+name);
+    ImageIO.write(segmentedImg, format, file1);
 
   }
 
